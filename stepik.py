aa8110d7e75c990044e65f5fbe37c9924c449e9e
RoleXX-A <alekcey123@gmail.com>
2023-05-13 15:44:25 +0300

    def vowels in string

--- stepik.py
+++ stepik.py
@@ -1,7 +1,9 @@
-str = 'vfjeolt'
-lst = ['e', 'o']
-lst1 = []
-for i in str:
-    if i in lst:
-        lst1.append(i)
-print(len(lst1))
+def vowels_in_string(str):
+    lst = ['e', 'o']
+    lst1 = []
+    for i in str:
+        if i in lst:
+            lst1.append(i)
+    print(len(lst1))
+
+vowels_in_string('foncms')
