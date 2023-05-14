461ad2a5476f9dfec95b4c9116864e39362468cd
RoleXX-A <alekcey123@gmail.com>
2023-05-14 13:36:03 +0300

    all vowels

--- stepik.py
+++ stepik.py
@@ -1,9 +1,10 @@
 def vowels_in_string(str):
-    lst = ['e', 'o']
+    lst = ['e', 'o', 'u', 'i', 'a','y','q', 'E','Y','U','I','O','A','Q']
     lst1 = []
     for i in str:
         if i in lst:
             lst1.append(i)
     print(len(lst1))
+
 
 vowels_in_string('foncms')
