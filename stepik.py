0b20b9d34317e5707b14a8738fdab345b63875f4
RoleXX-A <alekcey123@gmail.com>
2023-05-28 11:20:39 +0300

    проверка первой буквы с последней прудыдущего слова

--- stepik.py
+++ stepik.py
@@ -1,8 +1,14 @@
-num = int(input())
+city = list(map(str, input().lower().split()))
+end_city = ['ь', 'ъ', 'ы']
+out = 'ДА'
 
-for i in range(2, num):
-    if num % i == 0:
-        print('НЕТ')
-        break
-else:
-    print('ДА')
+for i in range(len(city)-1):
+    if city[i][-1] in end_city:
+        if city[i+1][0] != city[i][-2]:
+            out = 'НЕТ'
+            break
+    else:
+        if city[i+1][0] != city[i][-1]:
+            out = 'НЕТ'
+print(out)
+
