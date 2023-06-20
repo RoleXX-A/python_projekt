7490365948733da1d29f32e15a86343c2e970074
RoleXX-A <alekcey123@gmail.com>
2023-06-20 16:30:07 +0300

    1:поиск индекса фрагмента в строке 2:проверко правильности набора номера телефона

--- stepik.py
+++ stepik.py
@@ -1,12 +1,17 @@
-str_1 = input()
+# str_1 = input()
 
-if not 'ра' in str_1:
-    print(-1)
-else:
-    for i in range(0, len(str_1)):
-        if str_1[i] == 'р' and str_1[i+1] == 'а':
-            print(i, end=' ')
+# if not 'ра' in str_1:
+# print(-1)
+# else:
+# for i in range(0, len(str_1)):
+#   if str_1[i] == 'р' and str_1[i+1] == 'а':
+#      print(i, end=' ')
 
 
+number = input()
+number_1 = ['+', 7, '(', 'x', 'x', 'x', ')', 'x', 'x', 'x', '-', 'x', 'x', '-', 'x', 'x']
 
+if len(number) == len(number_1):
+    if number[0] == '+' and number_1[1] == 7:
+        print(number)
 
