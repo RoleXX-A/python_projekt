9172866bca39182039bd78c96e308b8ceeee952d
RoleXX-A <alekcey123@gmail.com>
2023-08-20 21:48:22 +0300

    tg

---
+++ TG_BOT.py
@@ -0,0 +1,5 @@
+import telebot
+
+secret_key = '6652756332:AAFuzhaFCwTis93M6uaOtZsfj6Q1kh369_k'
+
+bot = telebot.Telebot(secret_key)
--- stepik.py
+++ stepik.py
@@ -1,14 +1,10 @@
-city = list(map(str, input().lower().split()))
-end_city = ['ь', 'ъ', 'ы']
-out = 'ДА'
+def vowels_in_string(str):
+    lst = ['e', 'o', 'u', 'i', 'a','y','q', 'E','Y','U','I','O','A','Q']
+    lst1 = []
+    for i in str:
+        if i in lst:
+            lst1.append(i)
+    print(len(lst1))
 
-for i in range(len(city)-1):
-    if city[i][-1] in end_city:
-        if city[i+1][0] != city[i][-2]:
-            out = 'НЕТ'
-            break
-    else:
-        if city[i+1][0] != city[i][-1]:
-            out = 'НЕТ'
-print(out)
 
+vowels_in_string('foncms')
