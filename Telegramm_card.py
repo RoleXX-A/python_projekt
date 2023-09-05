32e0699e88e38aa54e5910ce3e339f697a297b9e
RoleXX-A <alekcey123@gmail.com>
2023-09-05 17:10:06 +0300

    телеграмм бот(доп)

--- Telegramm_card.py
+++ Telegramm_card.py
@@ -16,8 +16,24 @@ if __name__ == "__main__":
     rounds = 0
     card_number = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     card_suit = ['Ч', 'Б', 'Т', 'П']  # ч,б - красные, т,п - черные
+# мешаем карты
+    random_card_number = choice(card_number)
+    random_card_suit = choice(card_suit)
+    random_card = random_card_number + ' ' + random_card_suit
+
     if level == 1:
         print('Это легкий уровень, достаточно угадать только цвет')
+        while rounds < 5:
+            rounds += 1
+            player_answer = input('Выбери  карту: ')
+            if player_answer == 'черный' and random_card in ['Т', 'П']:
+                print('Ответ верный! Выбранная карта ' + random_card)
+
+            elif player_answer == 'красный' and random_card in ['Ч', 'Б']:
+                print('Ответ верный! Ваша карта ' + random_card)
+
+            else:
+                print('Ответ не верный! ' + random_card)
     elif level == 2:
         print('Это немного сложнее, угадай цвет и значение карты')
     elif level == 3:
@@ -25,20 +41,6 @@ if __name__ == "__main__":
     else:
         print('на таком уровне даже я не смогу угадать, выбери полегче.')
 
-    random_card_number = choice(card_number)
-    random_card_suit = choice(card_suit)
-    random_card = random_card_number + ' ' + random_card_suit
 
-    while rounds < 3:
-        rounds += 1
-        player_answer = input('Выбери  карту: ')
-        if player_answer == 'черный' and random_card_suit in ['Т', 'П']:
-            print('Ответ верный! Выбранная карта ' + random_card)
-
-        elif player_answer == 'красный' and random_card_suit in ['Ч', 'Б']:
-            print('Ответ верный! Ваша карта ' + random_card)
-
-        else:
-            print('Ответ не верный! ' + random_card)
 
     print('игра окончена')
