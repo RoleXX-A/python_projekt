c1aac189926c0362706874c851022a5f618e3ec8
RoleXX-A <alekcey123@gmail.com>
2024-01-15 16:40:29 +0300

    декоратор c параметрами

--- decorator 2.py
+++ decorator 2.py
@@ -1,16 +1,27 @@
-def type_int(function):
-    def wrapped(*args):
-        for arg in args:
-            if not isinstance(arg, int):
-                raise ValueError('Тип должен быть int')
-        return function(*args)
-    return wrapped
+def typed(type_):
+    def real_decorator(function):
+        def wrapped(*args):
+            for arg in args:
+                if not isinstance(arg, type_):
+                    raise ValueError(f'Тип должен быть {type_}')
+            return function(*args)
+
+        return wrapped
+
+    return real_decorator
 
 
-@type_int
-def calculate(a, b, c):
+@typed(int)
+def calculate(a, b, c):  # calculate = typed(int)(calculate)
     return a + b + c
+
+
+@typed(str)
+def convert(a, b): # convert = typed(str)(convert)
+    return a + b
 
 
 if __name__ == '__main__':
     print(calculate(1, 2, 3))
+    print(convert('1', 'hello'))
+
