310523e639539cd9dc84a598593c8a45ff00101c
RoleXX-A <alekcey123@gmail.com>
2024-11-24 10:12:40 +0300

    Создание таблицы OpenOffice

--- convert_value.py
+++ convert_value.py
@@ -1,10 +1,13 @@
 import requests
 
 url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+def convert_val():
+    response = requests.get(url)
+    data = response.json()
+    amount = int(input("Введите сумму: "))
+    to_currency = input("Введите валюту для перевода: ").upper()
+    result_convert = amount / data['Valute'][to_currency]['Value']
+    print(result_convert)
 
-response = requests.get(url)
-data = response.json()
-amount = int(input("Введите сумму: "))
-to_currency = input("Введите валюту для перевода: ").upper()
 
-print(amount/data['Valute'][to_currency]['Value'])
+convert_val()
